a0a57d7b1cac373b0b647194dcdaf21c8d6fbd52
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-11-03 17:11:01 -0400

    muck with oz

--- test/Goober.t.sol
+++ test/Goober.t.sol
@@ -4,18 +4,18 @@ pragma solidity ^0.8.15;
 import "forge-std/Test.sol";
 import "../src/interfaces/IERC20Metadata.sol";
 import "../src/Goober.sol";
-import "../src/proxy/ERC1967Proxy.sol";
+import "openzeppelin-contracts-upgradeable/proxy/ERC1967/ERC1967UpgradeUpgradeable.sol";
 
 contract TestUERC20Functionality is Test {
     using stdStorage for StdStorage;
 
     // Test Contracts
     Goober public goober_implementation;
-    ERC1967Proxy public goober_proxy;
+    ERC1967UpgradeUpgradeable public goober_proxy;
 
     function setUp() public {
         goober_implementation = new Goober();
-        goober_proxy = new ERC1967Proxy(address(goober_implementation),  abi.encodeWithSignature("initialize()"));
+        goober_proxy = new ERC1967UpgradeUpgradeable(address(goober_implementation),  abi.encodeWithSignature("initialize()"));
     }
 
     function test_proxy() public {
