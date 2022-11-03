8cd6b349f1748b7605263bd60db9d8be03c55e68
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-11-03 17:58:51 -0400

    eip-4626 jenga

--- src/Goober.sol
+++ src/Goober.sol
@@ -73,6 +73,9 @@ contract Goober is
 
     // TODO(Use safetransfers)
 
+    // Users need to be able to deposit and withdraw goo or gobblers
+    // Gobblers are valued by mult
+
     function deposit(uint256 assets, address receiver) public returns (uint256 shares) {
         // Check for rounding error since we round down in previewDeposit.
         require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
@@ -83,24 +86,9 @@ contract Goober is
         _mint(receiver, shares);
 
         emit Deposit(msg.sender, receiver, assets, shares);
-
-        afterDeposit(assets, shares);
     }
 
-    function mint(uint256 shares, address receiver) public returns (uint256 assets) {
-        assets = previewMint(shares); // No need to check for rounding error, previewMint rounds up.
-
-        // Need to transfer before minting or ERC777s could reenter.
-        goo.safeTransferFrom(msg.sender, address(this), assets);
-
-        _mint(receiver, shares);
-
-        emit Deposit(msg.sender, receiver, assets, shares);
-
-        afterDeposit(assets, shares);
-    }
-
-    function withdraw(uint256 assets, address receiver, address owner) public returns (uint256 shares) {
+    function withdraw(uint256 assets, address receiver, address owner) public virtual returns (uint256 shares) {
         shares = previewWithdraw(assets); // No need to check for rounding error, previewWithdraw rounds up.
 
         if (msg.sender != owner) {
@@ -108,27 +96,6 @@ contract Goober is
 
             if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
         }
-
-        beforeWithdraw(assets, shares);
-
-        _burn(owner, shares);
-
-        emit Withdraw(msg.sender, receiver, owner, assets, shares);
-
-        goo.safeTransfer(receiver, assets);
-    }
-
-    function redeem(uint256 shares, address receiver, address owner) public returns (uint256 assets) {
-        if (msg.sender != owner) {
-            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
-
-            if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
-        }
-
-        // Check for rounding error since we round down in previewRedeem.
-        require((assets = previewRedeem(shares)) != 0, "ZERO_ASSETS");
-
-        beforeWithdraw(assets, shares);
 
         _burn(owner, shares);
 
@@ -141,57 +108,21 @@ contract Goober is
         return 0;
     }
 
-    function convertToShares(uint256 assets) public view returns (uint256) {
-        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
-
-        return supply == 0 ? assets : assets.mulDivDown(supply, totalAssets());
-    }
-
-    function convertToAssets(uint256 shares) public view returns (uint256) {
-        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
-
-        return supply == 0 ? shares : shares.mulDivDown(totalAssets(), supply);
-    }
-
     function previewDeposit(uint256 assets) public view returns (uint256) {
-        return convertToShares(assets);
-    }
-
-    function previewMint(uint256 shares) public view returns (uint256) {
-        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
-
-        return supply == 0 ? shares : shares.mulDivUp(totalAssets(), supply);
+        return 0;
     }
 
     function previewWithdraw(uint256 assets) public view returns (uint256) {
-        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
-
-        return supply == 0 ? assets : assets.mulDivUp(supply, totalAssets());
-    }
-
-    function previewRedeem(uint256 shares) public view returns (uint256) {
-        return convertToAssets(shares);
+        return 0;
     }
 
     function maxDeposit(address) public pure returns (uint256) {
         return type(uint256).max;
     }
 
-    function maxMint(address) public pure returns (uint256) {
+    function maxWithdraw(address owner) public view returns (uint256) {
         return type(uint256).max;
     }
-
-    function maxWithdraw(address owner) public view returns (uint256) {
-        return convertToAssets(balanceOf[owner]);
-    }
-
-    function maxRedeem(address owner) public view returns (uint256) {
-        return balanceOf[owner];
-    }
-
-    function beforeWithdraw(uint256 assets, uint256 shares) internal {}
-
-    function afterDeposit(uint256 assets, uint256 shares) internal {}
 
     /**
      * @dev This empty reserved space is put in place to allow future versions to add new
