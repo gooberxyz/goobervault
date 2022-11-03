10fec3f998ef915762b66ced6c45c5e266475668
Alcibiades Athens <alcibiades.eth@protonmail.com>
2022-11-03 16:36:37 -0400

    start adding EIP-4626

--- src/Goober.sol
+++ src/Goober.sol
@@ -13,20 +13,26 @@ import "art-gobblers/ArtGobblers.sol";
 import "art-gobblers/Pages.sol";
 
 contract Goober is Initializable, UUPSUpgradeable, Ownable, Pausable, ReentrancyGuard, ERC20Upgradable {
-    // Events
-
-    event Deposit(address indexed caller, address indexed owner, uint256 assets, uint256 shares);
-
-    event Withdraw(
-        address indexed caller, address indexed receiver, address indexed owner, uint256 assets, uint256 shares
-    );
 
     // Constant/Immutable storage
 
     // TODO(Add casing for gorli deploy)
     Goo public constant goo = Goo(0x600000000a36F3cD48407e35eB7C5c910dc1f7a8);
     ArtGobblers public constant artGobblers = ArtGobblers(0x60bb1e2AA1c9ACAfB4d34F71585D7e959f387769);
-    Pages public constant pages = Pages(0x600Df00d3E42F885249902606383ecdcb65f2E02);
+
+    // EVENTS
+
+    event Deposit(address indexed caller, address indexed owner, uint256 assets, uint256 shares);
+
+    event Withdraw(
+        address indexed caller,
+        address indexed receiver,
+        address indexed owner,
+        uint256 assets,
+        uint256 shares
+    );
+
+    // Constructor/init
 
     constructor() initializer {}
 
@@ -40,6 +46,10 @@ contract Goober is Initializable, UUPSUpgradeable, Ownable, Pausable, Reentrancy
     // @dev required by the UUPS module
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
+    // G can be derived from Goo.totalSupply, plus the issuance rate
+    // M we can track internally
+    // Q must be tracked via an off chain oracle
+
     // When our GOO balance exceeds M/Q * G at time t where M is the total multiple of our GOBBLER and Q is the
     // total multiple of all GOBBLER and G is the total supply of GOO we have too much goo in the tank.
 
@@ -50,10 +60,141 @@ contract Goober is Initializable, UUPSUpgradeable, Ownable, Pausable, Reentrancy
     // total multiple of all GOBBLER and G is the total supply of GOO we have the right amount in the tank.
 
     // And, the events that can push us out of bounds are the deposit or withdraw of GOO/GOBBLER from
-    // the vault, mint of new GOBBLER (changes Q) or burn of a gobber
+    // the vault, mint of new GOBBLER (changes Q) or burn of gobblers from minting of legendary
 
-    // TODO(Effect of legendary gobblers)
     // TODO(Pages)
+    // TODO(Legendary gobblers)
+
+    // TODO(Views for goo and gobbler exchange rates to GBR)
+
+    function deposit(uint256 assets, address receiver) public virtual returns (uint256 shares) {
+        // Check for rounding error since we round down in previewDeposit.
+        require((shares = previewDeposit(assets)) != 0, "ZERO_SHARES");
+
+        // Need to transfer before minting or ERC777s could reenter.
+        goo.safeTransferFrom(msg.sender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        afterDeposit(assets, shares);
+    }
+
+    function mint(uint256 shares, address receiver) public virtual returns (uint256 assets) {
+        assets = previewMint(shares); // No need to check for rounding error, previewMint rounds up.
+
+        // Need to transfer before minting or ERC777s could reenter.
+        goo.safeTransferFrom(msg.sender, address(this), assets);
+
+        _mint(receiver, shares);
+
+        emit Deposit(msg.sender, receiver, assets, shares);
+
+        afterDeposit(assets, shares);
+    }
+
+    function withdraw(
+        uint256 assets,
+        address receiver,
+        address owner
+    ) public virtual returns (uint256 shares) {
+        shares = previewWithdraw(assets); // No need to check for rounding error, previewWithdraw rounds up.
+
+        if (msg.sender != owner) {
+            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
+        }
+
+        beforeWithdraw(assets, shares);
+
+        _burn(owner, shares);
+
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+
+        goo.safeTransfer(receiver, assets);
+    }
+
+    function redeem(
+        uint256 shares,
+        address receiver,
+        address owner
+    ) public virtual returns (uint256 assets) {
+        if (msg.sender != owner) {
+            uint256 allowed = allowance[owner][msg.sender]; // Saves gas for limited approvals.
+
+            if (allowed != type(uint256).max) allowance[owner][msg.sender] = allowed - shares;
+        }
+
+        // Check for rounding error since we round down in previewRedeem.
+        require((assets = previewRedeem(shares)) != 0, "ZERO_ASSETS");
+
+        beforeWithdraw(assets, shares);
+
+        _burn(owner, shares);
+
+        emit Withdraw(msg.sender, receiver, owner, assets, shares);
+
+        goo.safeTransfer(receiver, assets);
+    }
+
+    function totalAssets() public view virtual returns (uint256);
+
+    function convertToShares(uint256 assets) public view virtual returns (uint256) {
+        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+
+        return supply == 0 ? assets : assets.mulDivDown(supply, totalAssets());
+    }
+
+    function convertToAssets(uint256 shares) public view virtual returns (uint256) {
+        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+
+        return supply == 0 ? shares : shares.mulDivDown(totalAssets(), supply);
+    }
+
+    function previewDeposit(uint256 assets) public view virtual returns (uint256) {
+        return convertToShares(assets);
+    }
+
+    function previewMint(uint256 shares) public view virtual returns (uint256) {
+        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+
+        return supply == 0 ? shares : shares.mulDivUp(totalAssets(), supply);
+    }
+
+    function previewWithdraw(uint256 assets) public view virtual returns (uint256) {
+        uint256 supply = totalSupply; // Saves an extra SLOAD if totalSupply is non-zero.
+
+        return supply == 0 ? assets : assets.mulDivUp(supply, totalAssets());
+    }
+
+    function previewRedeem(uint256 shares) public view virtual returns (uint256) {
+        return convertToAssets(shares);
+    }
+
+    function maxDeposit(address) public view virtual returns (uint256) {
+        return type(uint256).max;
+    }
+
+    function maxMint(address) public view virtual returns (uint256) {
+        return type(uint256).max;
+    }
+
+    function maxWithdraw(address owner) public view virtual returns (uint256) {
+        return convertToAssets(balanceOf[owner]);
+    }
+
+    function maxRedeem(address owner) public view virtual returns (uint256) {
+        return balanceOf[owner];
+    }
+
+    function beforeWithdraw(uint256 assets, uint256 shares) internal virtual {}
+
+    function afterDeposit(uint256 assets, uint256 shares) internal virtual {}
+
+
+
 
     /**
      * @dev This empty reserved space is put in place to allow future versions to add new
