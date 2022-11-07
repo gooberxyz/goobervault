150199d9c9fc6f10f8c2e12f7f3feef0e4bdde6c
bandit <mevbandit@gmail.com>
2022-11-06 19:19:40 -0800

    Update Goober.sol

--- src/Goober.sol
+++ src/Goober.sol
@@ -38,6 +38,9 @@ contract Goober is
 
     // Mutable storage
 
+    // Keeps track of count of each mult in vault, updated by _update().
+    uint8[] memory multsCount = new uint8[](5)
+
     // Accumulators
     uint256 public priceGooCumulativeLast;
     uint256 public priceGobblerCumulativeLast;
@@ -70,6 +73,13 @@ contract Goober is
         address indexed to
     );
 
+    // Emitted by getMoreGobblers(). If minted == false, it means the gobbler was purchased. 
+    event Rebalance(
+          uint256 gobblersMultIn,
+          uint256 gobblerOverflow, 
+          bool minted,
+    );
+
     event Sync(uint112 gooBalance, uint112 multBalance);
 
     // Constructor/init
@@ -89,16 +99,18 @@ contract Goober is
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     // update reserves and, on the first call per block, price accumulators
-    function _update(uint256 gooBalance, uint256 gobblerBalance, uint112 _gooReserve, uint112 _gobblerReserve)
+    // TODO: Update the multsCount array. 
+
+    function _update(uint256 gooBalance, uint256 gobblerBalance, uint112 _gooReserve, uint112 _multReserve)
         private
     {
         require(gooBalance <= type(uint112).max && gobblerBalance <= type(uint112).max, "Goober: OVERFLOW");
         uint40 blockTimestamp = uint40(block.timestamp % 2 ** 40);
         uint40 timeElapsed = blockTimestamp - blockTimestampLast; // overflow is desired
-        if (timeElapsed > 0 && _gooReserve != 0 && _gobblerReserve != 0) {
+        if (timeElapsed > 0 && _gooReserve != 0 && _multReserve != 0) {
             // * never overflows, and + overflow is desired
-            priceGooCumulativeLast += uint256(UQ112x112.encode(_gobblerReserve).uqdiv(_gooReserve)) * timeElapsed;
-            priceGobblerCumulativeLast += uint256(UQ112x112.encode(_gooReserve).uqdiv(_gobblerReserve)) * timeElapsed;
+            priceGooCumulativeLast += uint256(UQ112x112.encode(_multReserve).uqdiv(_gooReserve)) * timeElapsed;
+            priceGobblerCumulativeLast += uint256(UQ112x112.encode(_gooReserve).uqdiv(_multReserve)) * timeElapsed;
         }
         // TODO(Do we need any special magic here)
         //reserve0 = uint112(gooBalance);
@@ -240,11 +252,38 @@ contract Goober is
     function getReserves()
         public
         view
-        returns (uint112 _gooReserve, uint112 _gobblerReserve, uint40 _blockTimestampLast)
+        returns (uint112 _gooReserve, uint112 _multReserve, uint40 _blockTimestampLast)
     {
         _gooReserve = uint112(artGobblers.gooBalance(address(this)));
-        _gobblerReserve = uint112(artGobblers.getUserEmissionMultiple(address(this))) * 1000;
+        _multReserve = uint112(artGobblers.getUserEmissionMultiple(address(this))) * 1000;
         _blockTimestampLast = blockTimestampLast;
+    }
+
+    /**
+        @notice Returns all Gobbler IDs held by the pool
+     */
+    function getGobblerIds() external view virtual returns (uint256[] memory);
+
+    /**
+        @notice Returns the quantity of Gobblers of each mult held by the pool. 
+     */
+    function getGobblerCount() external view virtual returns (uint256[] memory);
+
+
+     /**
+        @notice Determines whether to mint or buy based on current floor prices of each mult
+        @notice compared to current mint price determined by the VRGDA, by calling artGobblers.gobblerPrice()
+     */
+    function _mintOrBuy() internal returns (bool) {
+
+    }
+
+    // Will need to call _update() to update reserves of Gobblers and Goo (upon success). 
+    // Will need to remove the Goo from the Gobblers (virtual), in order to 
+    // either mint a Gobbler by burning Goo, or sell the Goo for Eth and buying a Gobbler
+    // off secondary. 
+    function getMoreGobblers() public nonReentrant {
+        require _mintOrBuy()
     }
 
     // this low-level function should be called from a contract which performs important safety checks
@@ -267,8 +306,8 @@ contract Goober is
             }
             require(gooTokens > 0 || multOut > 0, "Goober: INSUFFICIENT_OUTPUT_AMOUNT");
         }
-        (uint112 _gooReserve, uint112 _gobblerReserve,) = getReserves(); // gas savings
-        require(gooTokens < _gooReserve && multOut < _gobblerReserve, "Goober: INSUFFICIENT_LIQUIDITY");
+        (uint112 _gooReserve, uint112 _multReserve,) = getReserves(); // gas savings
+        require(gooTokens < _gooReserve && multOut < _multReserve, "Goober: INSUFFICIENT_LIQUIDITY");
         uint256 gooBalance;
         uint256 gobblerBalance;
         {
@@ -299,7 +338,7 @@ contract Goober is
         }
         uint256 amount0In = gooBalance > _gooReserve - gooTokens ? gooBalance - (_gooReserve - gooTokens) : 0;
         uint256 amount1In =
-            gobblerBalance > _gobblerReserve - multOut ? gobblerBalance - (_gobblerReserve - multOut) : 0;
+            gobblerBalance > _multReserve - multOut ? gobblerBalance - (_multReserve - multOut) : 0;
         require(amount0In > 0 || amount1In > 0, "Goober: INSUFFICIENT_INPUT_AMOUNT");
         {
             // scope for reserve{0,1}Adjusted, avoids stack too deep errors
@@ -308,10 +347,10 @@ contract Goober is
             uint256 balance0Adjusted = (gooBalance * 1000) - (amount0In * 3);
             //uint256 balance1Adjusted = (gobblerBalance * 1000) - (amount1In * 3);
             require(
-                (balance0Adjusted * gobblerBalance) >= (uint256(_gooReserve) * _gobblerReserve * 1000 ** 2), "Goober: K"
+                (balance0Adjusted * gobblerBalance) >= (uint256(_gooReserve) * _multReserve * 1000 ** 2), "Goober: K"
             );
         }
-        _update(gooBalance, gobblerBalance, _gooReserve, _gobblerReserve);
+        _update(gooBalance, gobblerBalance, _gooReserve, _multReserve);
         emit Swap(msg.sender, amount0In, amount1In, gooTokens, multOut, to);
     }
 
