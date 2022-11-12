2a1568bbdcc47f00e867d9469c4bbdd2b45e22b8
horsefacts <webmaster@terminally.online>
2022-11-12 18:48:17 -0500

    Update actors, draw gobblers from pool

--- test/Goober.invariants.t.sol
+++ test/Goober.invariants.t.sol
@@ -23,6 +23,8 @@ abstract contract InvariantActor is StdUtils, CommonBase {
     RandProvider internal randProvider;
     VRFCoordinatorMock internal vrfCoordinator;
 
+    uint256[] internal gobblerPool;
+
     constructor(
         Goober _goober,
         Goo _goo,
@@ -36,13 +38,37 @@ abstract contract InvariantActor is StdUtils, CommonBase {
         randProvider = _randProvider;
         vrfCoordinator = _vrfCoordinator;
 
+        // Approve all goo to Goober
         goo.approve(address(goober), type(uint256).max);
+
+        // Approve all gobbler to Goober
         gobblers.setApprovalForAll(address(goober), true);
+
+        // Mint a pool of 300 Gobblers. Actors can draw from
+        // this pool when they need Gobblers to swap/deposit.
+        gobblerPool = _mintGobblers(address(this), 300);
+
+        // Warp forward a full year, since the VRGDA price
+        // of Gobblers is now very high.
+        vm.warp(block.timestamp + 365 days);
     }
 
-    event GobblerPrice(uint256 price);
+    function _drawGobblers(uint256 numGobblers) internal returns (uint256[] memory) {
+        uint256[] memory gobblerIds = new uint256[](numGobblers);
+        for (uint256 i; i < numGobblers; i++) {
+            gobblerIds[i] = gobblerPool[gobblerPool.length - 1];
+            gobblerPool.pop();
+        }
+        return gobblerIds;
+    }
 
-    function _mintGobblers(address who, uint256 numGobblers) internal returns (uint256[] memory) {
+    function _returnGobblers(uint256[] memory gobblerIds) internal {
+        for (uint256 i; i < gobblerIds.length; i++) {
+            gobblerPool.push(gobblerIds[i]);
+        }
+    }
+
+    function _mintGobblers(address who, uint256 numGobblers) private returns (uint256[] memory) {
         uint256[] memory mintedGobblers = new uint256[](numGobblers);
         if (numGobblers == 0) return mintedGobblers;
         for (uint256 i = 0; i < numGobblers; i++) {
@@ -63,7 +89,7 @@ abstract contract InvariantActor is StdUtils, CommonBase {
     }
 
     /// @dev Call back vrf with randomness and reveal gobblers.
-    function _setRandomnessAndReveal(uint256 numReveal, string memory seed) internal {
+    function _setRandomnessAndReveal(uint256 numReveal, string memory seed) private {
         bytes32 requestId = gobblers.requestRandomSeed();
         uint256 randomness = uint256(keccak256(abi.encodePacked(seed)));
         // call back from coordinator
@@ -72,7 +98,18 @@ abstract contract InvariantActor is StdUtils, CommonBase {
     }
 }
 
-contract InvariantSwapper is InvariantActor {
+/// @dev The InvariantWarper warps forward a random number of days.
+contract InvariantWarper is CommonBase {
+    function warp(uint8 numDays) external {
+        vm.warp(block.timestamp + uint256(numDays) * 1 days);
+    }
+}
+
+/// @dev The InvariantAdmin randomly mints Gobblers and skims.
+contract InvariantAdmin is InvariantActor {
+    address internal constant FEE_TO = address(0xFEEE);
+    address internal constant MINTER = address(0x1337);
+
     constructor(
         Goober _goober,
         Goo _goo,
@@ -81,17 +118,28 @@ contract InvariantSwapper is InvariantActor {
         VRFCoordinatorMock _vrfCoordinator
     ) InvariantActor(_goober, _goo, _gobblers, _randProvider, _vrfCoordinator) {}
 
-    function swap(uint8 gooIn, uint8 swapGobblers, uint8 poolGobblers, uint8 gooOut) external {
-        uint256[] memory gobblersIn = _mintGobblers(address(this), swapGobblers);
-        uint256[] memory gobblersOut = _mintGobblers(address(goober), poolGobblers);
+    function mint() external {
+        vm.prank(MINTER);
+        goober.mintGobbler();
+    }
 
-        _writeTokenBalance(address(this), address(goo), gooIn);
-
-        goober.swap(gobblersOut, gooOut, gobblersIn, gooIn, address(this), bytes(""));
+    function skim() external {
+        vm.prank(FEE_TO);
+        goober.skimGoo();
     }
 }
 
-contract InvariantLP is InvariantActor {
+/// @dev The InvariantUser calls deposit, withdraw, and swap.
+contract InvariantUser is InvariantActor {
+    uint256 depositedGoo;
+    uint256[] depositedGobblers;
+
+    uint256 public depositCalls;
+    uint256 public withdrawCalls;
+    uint256 public swapGobblersForGobblersCalls;
+    uint256 public swapGooForGobblersCalls;
+    uint256 public swapGobblersForGooCalls;
+
     constructor(
         Goober _goober,
         Goo _goo,
@@ -101,17 +149,164 @@ contract InvariantLP is InvariantActor {
     ) InvariantActor(_goober, _goo, _gobblers, _randProvider, _vrfCoordinator) {}
 
     function deposit(uint8 _gooTokens, uint8 _numGobblers) external {
-        // Convert to wad — range will be 0-255 GOO
+        // convert _gooTokens to an amount between 10 and 2550 Goo.
         uint256 gooTokens = uint256(_gooTokens) * 10 ether;
 
-        // Limit Gobbler mints to 7. This prevents the VRGDA price from increasing
-        // so high it overflows later calculations.
-        uint256 numGobblers = _numGobblers % 8;
+        // convert _numGobblers to an amount between 0-10.
+        uint256 numGobblers = _numGobblers % 11;
 
-        uint256[] memory gobblersIn = _mintGobblers(address(this), numGobblers);
+        // Draw gobblers from the pool
+        uint256[] memory gobblersIn = _drawGobblers(numGobblers);
+
+        // Mint ourselves Goo equal to the deposit amount
         _writeTokenBalance(address(this), address(goo), gooTokens);
 
+        // Account for deposited goo and gobblers.
+        for (uint256 i; i < gobblersIn.length; i++) {
+            depositedGobblers.push(gobblersIn[i]);
+        }
+        depositedGoo += gooTokens;
+
+        // Perform deposit
         goober.deposit(gobblersIn, gooTokens, address(this));
+    }
+
+    function withdraw(uint8 _gooNum, uint8 _gobblerNum) external {
+        // Use _gooNum to calculate the proportion of deposited goo to withdraw.
+        uint256 gooTokens = depositedGoo * uint256(_gooNum) / type(uint8).max;
+
+        // Use _gobblerNum to calculate the proportion of deposited gobblers to withdraw.
+        uint256 numGobblers = depositedGobblers.length * uint256(_gobblerNum) / type(uint8).max;
+
+        // Remove gobblers from the depositedGobblers array.
+        uint256[] memory gobblersOut = new uint256[](numGobblers);
+        for (uint256 i; i < numGobblers; i++) {
+            gobblersOut[i] = depositedGobblers[depositedGobblers.length - 1];
+            depositedGobblers.pop();
+        }
+        // Account for withdrawn goo.
+        depositedGoo -= gooTokens;
+
+        // Return removed gobblers to the pool.
+        _returnGobblers(gobblersOut);
+
+        // Perform withdrawal
+        goober.withdraw(gobblersOut, gooTokens, address(this), address(this));
+    }
+
+    function swapGobblersForGobblers(uint8 _swapGobblers, uint8 _poolGobblers) external {
+        // Convert _swapGobblers to a number between 1-10.
+        // Draw this many gobblers from the pool.
+        uint256 swapGobblers = _swapGobblers % 10 + 1;
+        uint256[] memory gobblersIn = _drawGobblers(swapGobblers);
+
+        // Convert _poolGobblers to a number between 1-10
+        uint256 poolGobblers = _poolGobblers % 10 + 1;
+
+        // We can't swap for more gobblers than are in the Goober pool
+        poolGobblers = poolGobblers > depositedGobblers.length ? depositedGobblers.length : poolGobblers;
+
+        // Get the ids of gobblers we know are deposited
+        uint256[] memory gobblersOut = new uint256[](poolGobblers);
+        for (uint256 i; i < poolGobblers; i++) {
+            gobblersOut[i] = depositedGobblers[depositedGobblers.length - 1];
+            depositedGobblers.pop();
+        }
+
+        // Add the gobblers we're swapping to the depositedGobblers array
+        for (uint256 i; i < swapGobblers; i++) {
+            depositedGobblers.push(gobblersIn[i]);
+        }
+
+        // Preview the swap to determine how much Goo we need to provide.
+        int256 erroneousGoo =
+            goober.previewSwap({gobblersOut: gobblersOut, gooOut: 0, gobblersIn: gobblersIn, gooIn: 0});
+
+        // If we need to provide more goo, mint it
+        if (erroneousGoo > 0) {
+            _writeTokenBalance(address(this), address(goo), uint256(erroneousGoo));
+        }
+        uint256 gooIn = (erroneousGoo > 0) ? uint256(erroneousGoo) : 0;
+
+        // Perform the swap
+        goober.swap({
+            gobblersOut: gobblersOut,
+            gooOut: 0,
+            gobblersIn: gobblersIn,
+            gooIn: gooIn,
+            receiver: address(this),
+            data: bytes("")
+        });
+    }
+
+    function swapGobblersForGoo(uint8 _swapGobblers) external {
+        // Convert _swapGobblers to a number between 1-10.
+        // Draw this many gobblers from the pool.
+        uint256 swapGobblers = _swapGobblers % 10 + 1;
+        uint256[] memory gobblersIn = _drawGobblers(swapGobblers);
+
+        // Push these gobblers in to the depositedGobblers array
+        for (uint256 i; i < swapGobblers; i++) {
+            depositedGobblers.push(gobblersIn[i]);
+        }
+
+        // No gobblersOut, since we are swapping for Goo only
+        uint256[] memory gobblersOut = new uint256[](0);
+
+        // Preview the swap to determine how much Goo we expect
+        int256 erroneousGoo =
+            goober.previewSwap({gobblersOut: gobblersOut, gooOut: 1, gobblersIn: gobblersIn, gooIn: 0});
+
+        // erroneousGoo will be negative, convert to a positive amount
+        uint256 gooOut = (erroneousGoo < 0) ? uint256(-1 * erroneousGoo) : 0;
+
+        // Perform the swap
+        goober.swap({
+            gobblersOut: gobblersOut,
+            gooOut: gooOut,
+            gobblersIn: gobblersIn,
+            gooIn: 0,
+            receiver: address(this),
+            data: bytes("")
+        });
+    }
+
+    function swapGooForGobblers(uint8 _poolGobblers) external {
+        // No gobblersIn for this swap.
+        uint256[] memory gobblersIn = new uint256[](0);
+
+        // Convert _poolGobblers to a number between 1-10
+        uint256 poolGobblers = _poolGobblers % 10 + 1;
+
+        // We can't swap for more gobblers than are in the Goober pool
+        poolGobblers = poolGobblers > depositedGobblers.length ? depositedGobblers.length : poolGobblers;
+
+        // Get the ids of gobblers we know are deposited
+        uint256[] memory gobblersOut = new uint256[](poolGobblers);
+        for (uint256 i; i < poolGobblers; i++) {
+            gobblersOut[i] = depositedGobblers[depositedGobblers.length - 1];
+            depositedGobblers.pop();
+        }
+
+        // Preview the swap to work out how much Goo we need to provide
+        int256 erroneousGoo =
+            goober.previewSwap({gobblersOut: gobblersOut, gooOut: 0, gobblersIn: gobblersIn, gooIn: 1});
+
+        // Calculate gooIn based on the preview, and mint Goo to swap.
+        if (erroneousGoo > 0) {
+            _writeTokenBalance(address(this), address(goo), uint256(erroneousGoo));
+        }
+        uint256 gooIn = (erroneousGoo > 0) ? uint256(erroneousGoo) : 0;
+
+        // Perform the swap
+        goober.swap({
+            gobblersOut: gobblersOut,
+            gooOut: 0,
+            gobblersIn: gobblersIn,
+            gooIn: gooIn,
+            receiver: address(this),
+            data: bytes("")
+        });
     }
 }
 
@@ -134,8 +329,9 @@ contract GooberInvariantsTest is Test {
     GobblerReserve internal community;
     RandProvider internal randProvider;
 
-    InvariantSwapper internal swapper;
-    InvariantLP internal lp;
+    InvariantUser internal user;
+    InvariantAdmin internal admin;
+    InvariantWarper internal warper;
 
     bytes32 private keyHash;
     uint256 private fee;
@@ -146,6 +342,9 @@ contract GooberInvariantsTest is Test {
     uint256 internal constant TIME0 = 2_000_000_000; // now-ish unix timestamp
 
     address[] private _targetContracts;
+
+    uint256 prevGooBalance;
+    uint256 prevGbrFeeBalance;
 
     function _addTargetContract(address newTargetContract_) internal {
         _targetContracts.push(newTargetContract_);
@@ -209,7 +408,7 @@ contract GooberInvariantsTest is Test {
             _minter: MINTER
         });
 
-        swapper = new InvariantSwapper({
+        user = new InvariantUser({
             _goo: goo,
             _gobblers: gobblers,
             _goober: goober,
@@ -217,7 +416,7 @@ contract GooberInvariantsTest is Test {
             _randProvider: randProvider
         });
 
-        lp = new InvariantLP({
+        admin = new InvariantAdmin({
             _goo: goo,
             _gobblers: gobblers,
             _goober: goober,
@@ -225,16 +424,25 @@ contract GooberInvariantsTest is Test {
             _randProvider: randProvider
         });
 
-        _addTargetContract(address(lp));
-        //_addTargetContract(address(swapper));
+        warper = new InvariantWarper();
+
+        _addTargetContract(address(user));
+        _addTargetContract(address(admin));
+        _addTargetContract(address(warper));
     }
 
-    function invariant_balances() public {
-        assertEq(gobblers.gooBalance(address(goober)), 0);
+    function invariant_goo_balance() public {
+        // I am surprised this passes: isn't it possible for goo
+        // balances to decrease with withdrawals?
+        uint256 gooBalance = gobblers.gooBalance(address(goober));
+        assertGe(gooBalance, prevGooBalance);
+        prevGooBalance = gooBalance;
     }
 
-    function test_concrete() public {
-        lp.deposit(254, 255);
+    function invariant_fee_balance() public {
+        uint256 gbrFeeBalance = goober.balanceOf(FEE_TO);
+        assertGe(gbrFeeBalance, prevGbrFeeBalance);
+        prevGbrFeeBalance = gbrFeeBalance;
     }
 
     /*//////////////////////////////////////////////////////////////
